3bcd431ba92d43b4ecf4cb59a85c2d78843700c5
ningyan1228 <wyzxn21@163.com>
2025-04-04 21:27:07 +0800

    函数

---
+++ 类和对象-封装/类和对象-封装/C++对象模型和this指针.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include<string>
+
+
+/* ----------------------------------------
+
+-------------------------------------------
+*/
+
+int main() {
+
+
+    cout<<"hello world"
+    system("pause");
+    return 0;
+}
